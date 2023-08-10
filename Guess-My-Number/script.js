1bf3be55b7b20790c4b195343e15cc1ee80fec8d
veekay17 <vijay.vuex@gmail.com>
2023-08-10 20:16:01 +0530

    project completed

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -3,30 +3,62 @@ console.log("hi")
 const againBtn = document.querySelector(".btn.again")
 const checkBtn = document.querySelector(".btn.check")
 let numberToGuess = document.querySelector(".number")
-let messageForGuess = document.querySelector(".message")
+// let messageForGuess = 
 let scoreText = document.querySelector(".score")
+const highscoreText = document.querySelector(".highscore")
 let scoreValue = 20;
+let highScore = 0;
 
 
 let randomNumber = Math.floor((Math.random()*20 ) + 1)
+
+const displayMessage = (message) => {
+    document.querySelector(".message").textContent = message
+} 
+console.log(randomNumber)
 checkBtn.addEventListener("click", (e) => {
     let input = parseInt(document.querySelector(".guess").value)
+    if(!input){
+        displayMessage("Please enter a number")
+    }
+    if(input){
     if(input === randomNumber){
-       messageForGuess.innerText = "You are correct 🎉"
+       displayMessage("You are correct 🎉")
+       highScore = scoreValue
        document.body.style.backgroundColor = "#60b347"
+       checkBtn.disabled = true
+       checkBtn.style.color = "red"
     }else if(input < randomNumber){
-        messageForGuess.innerText = "Too Low "
+       displayMessage("Too Low ")
         scoreValue--
         
     }else{
-        messageForGuess.innerText = "Too high"
+       displayMessage("Too high")
         scoreValue--
     }
+}
     if(scoreValue >= 0){
 
         scoreText.innerText = scoreValue
+        highscoreText.innerText = highScore
     }else{
         messageForGuess.innerText = "Game Over"
         document.body.style.backgroundColor = "red"
     }
+})
+
+againBtn.addEventListener("click", function(){
+    scoreValue = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(randomNumber)
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = scoreValue;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  checkBtn.disabled = false
 })
