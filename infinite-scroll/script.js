8b7a032431f628688b64905e1aa5f70fafa3acbe
Iamthevk <vijay.vuex@gmail.com>
2023-09-20 23:28:19 +0530

    fetching new photos on scrolling and after loading initial 30 photos

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,25 +1,35 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
-const count = 30;
 let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+let allImageLoaded = false;
 
 // Unsplash API
 // Normally, don't store API Keys like this, but an exception made here because it is free, and the data is publicly available!
-const apiKey = "-KsOXvdzrmis5K5FFHbUNb1HOJks2OpVmYARueRdjAw";
+const count = 30;
+const apiKey = "QTh-XZKMsc3wSPAPsNs5Y9wK0odx7-UoME6SHcju2Hg";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&orientation=landscape&topics=nature`;
-let photosArray = [];
 
 //get photos from unsplash api
 async function getPhotos() {
   const response = await fetch(apiUrl);
   photosArray = await response.json();
-  console.log(photosArray);
   displayPhotos();
+}
+
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    allImageLoaded = true;
+    loader.hidden = true;
+  }
 }
 
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
   imagesLoaded = 0;
+  totalImages = photosArray.length;
   photosArray.forEach((photo) => {
     const a = document.createElement("a");
     setAttributes(a, {
@@ -33,11 +43,9 @@ function displayPhotos() {
     });
     a.appendChild(image);
     imageContainer.appendChild(a);
-    loader.hidden = true;
+
     // Event Listener, check when each is finished loading
-    image.addEventListener("load", () => {
-      console.log("image loaded");
-    });
+    image.addEventListener("load", imageLoaded);
   });
 }
 //on first time load
@@ -51,15 +59,14 @@ function setAttributes(element, attributes) {
 }
 
 function fetchNewPhotos() {
-  // console.log(window.scrollY, "scrollY");
-  // console.log(window.innerHeight, "innerheight");
-  // console.log(document.body.offsetHeight);
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
-    // getPhotos();
-    console.log(window.scrollY + window.innerHeight, "scrollY");
-    console.log(window.innerHeight, "innerheight");
-    console.log(document.body.offsetHeight);
-    console.log("load more");
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    allImageLoaded
+  ) {
+    allImageLoaded = false;
+    getPhotos();
+    // console.log(window.scrollY + window.innerHeight);
+    // console.log(document.body.offsetHeight);
   }
 }
-// window.addEventListener("scroll", fetchNewPhotos);
+window.addEventListener("scroll", fetchNewPhotos);
