aeb096026595ca174e95a58f20e25b72bdf9eb0a
Iamthevk <vijay.vuex@gmail.com>
2023-12-28 20:30:50 +0530

    addFruit functionality add

--- fruits-store/script.js
+++ fruits-store/script.js
@@ -8,8 +8,49 @@ const container = document.querySelector(".fruit-container");
 const list = document.querySelector(".fruit-list");
 const clearBtn = document.querySelector(".clear-btn");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", addFruit);
+
+function addFruit(e) {
   e.preventDefault();
   const inputValue = fruit.value;
   //   list.textContent = inputValue;
-});
+  const id = new Date().getTime().toString();
+
+  if (inputValue !== "") {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("fruit-item");
+    console.log(element);
+    element.innerHTML = `<p class="title">${inputValue}</p>
+      <div class="btn-container">
+        <!-- edit btn -->
+        <button type="button" class="edit-btn">
+          <i class="fas fa-edit"></i>
+        </button>
+        <!-- delete btn -->
+        <button type="button" class="delete-btn">
+          <i class="fas fa-trash"></i>
+        </button>
+      </div>
+    `;
+    list.appendChild(element);
+    container.classList.add("show-container");
+    displayAlert("item added to the list", "success");
+    fruit.value = "";
+  } else {
+    displayAlert("please enter value", "danger");
+  }
+}
+
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  // remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
